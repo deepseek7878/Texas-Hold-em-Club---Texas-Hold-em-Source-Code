91c545ec0f75e4e99d05f2ece9b565ab24d58adc
deepseek7878 <lihongbo9414@gmail.com>
2026-05-25 20:51:14 +0800

    Add files via upload
    server code

---
+++ defer.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "common/nndef.h"
+
+using namespace nndef;
+
+namespace game
+{
+    class GameRoot;
+
+    namespace logic
+    {
+        namespace gamelogic
+        {
+            int Defer(GameRoot *root);
+        }
+    }
+}
+
